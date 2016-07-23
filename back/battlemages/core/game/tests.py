9afe00bf0cf0c5c6e78d1bf4affbb3dcd5f1f28f
Vincent Desprez <vincent.desprez@gmail.com>
2016-07-23 23:19:00 +0200

    WIP

--- back/battlemages/core/game/tests.py
+++ back/battlemages/core/game/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
 from battlemages.core.mages.models import Mage
+from battlemages.core.players.models import Player
 from .models import Game
 
-class GameTestCase(TestCase):
+class InitGameTestCase(TestCase):
+    "tests about setting up and destroying the game"
     fixtures = ['test_fixtures.yaml']
 
     def testFixtures(self):
         m = Mage.objects.first()
         print(m.name)
+
+    def testNewGame(self):
+        termi = Mage.objects.get(name="Termifire")
+        omega = Mage.objects.get(name="OmegaMax")
+        toto = Player.objects.get(username="toto")
+        titi = Player.objects.get(username="titi")
