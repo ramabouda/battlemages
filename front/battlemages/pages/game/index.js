a921a106cf388a448289913324b1d3e672979e81
raphael.boucher <raphael.boucher@polyconseil.fr>
2016-07-31 19:33:38 +0200

    Add logout from game

--- front/battlemages/pages/game/index.js
+++ front/battlemages/pages/game/index.js
@@ -1,7 +1,23 @@
+import Vue from 'vue'
+
 import {Authentication} from 'battlemages/core/auth'
+import template from './template.jade'
 
 
 const auth = new Authentication()
 if (!auth.isAuthenticated()) {
   window.location = './login'
 }
+
+
+export const gameVue = new Vue({
+  el: '#app',
+  template: template(),
+  data: {},
+  methods: {
+    logout: function () {
+      auth.logout()
+      window.location = './login'
+    },
+  },
+})
