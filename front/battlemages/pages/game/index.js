2bb0f6f4ae8b3fda8855782daff230c23bb7f308
raphael.boucher <raphael.boucher@polyconseil.fr>
2016-08-05 00:50:32 +0200

    Start getting data with WS

--- front/battlemages/pages/game/index.js
+++ front/battlemages/pages/game/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import ReconnectingWebSocket from 'reconnecting-websocket'
 
 import {Authentication} from 'battlemages/core/auth'
 import template from './template.jade'
@@ -10,10 +11,20 @@ if (!auth.isAuthenticated()) {
 }
 
 
+const ws = new ReconnectingWebSocket('ws://127.0.0.1:8000/ws/?token=' + auth.token)
+
+const connectedUsers = {}
+ws.onmessage = function (response) {
+  const joined = JSON.parse(response.data)
+  Object.keys(joined).forEach(k => Vue.set(connectedUsers, k, joined[k]))
+}
+
 export const gameVue = new Vue({
   el: '#app',
   template: template(),
-  data: {},
+  data: {
+    connectedUsers: connectedUsers,
+  },
   methods: {
     logout: function () {
       auth.logout()
