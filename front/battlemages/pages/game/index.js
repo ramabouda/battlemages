02ace3d2e03950aa527d53c70ace57aae7479135
raphael.boucher <raphael.boucher@polyconseil.fr>
2016-09-11 20:14:18 +0200

    Create multiplexer and consumer in JS

--- front/battlemages/pages/game/index.js
+++ front/battlemages/pages/game/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import ReconnectingWebSocket from 'reconnecting-websocket'
+import {MultiplexedWebsocket, MultiplexedConsumer} from 'battlemages/core/consumer'
 
 import {Authentication} from 'battlemages/core/auth'
 import template from './template.jade'
@@ -13,24 +14,21 @@ if (!auth.isAuthenticated()) {
 const connectedUsers = {}
 
 const ws = new WebSocket('ws://127.0.0.1:8000/ws/')
-ws.onclose = function () {
+
+// Move this to a root file
+const demultiplexer = new MultiplexedWebsocket(ws)
+
+const presenceConsumer = new MultiplexedConsumer('presence')
+
+presenceConsumer.connect = function () {
+  this.send({'toto': 'tata'})
 }
 
-ws.onopen = function () {
-  window.ws = ws
-  ws.send(JSON.stringify({
-    order: 1,
-    stream: 'presence',
-    payload: {
-      'toto': 'tata',
-    },
-  }))
+presenceConsumer.receive = function (message) {
+  Object.keys(message).forEach(k => Vue.set(connectedUsers, k, message[k]))
 }
 
-ws.onmessage = function (response) {
-  const joined = JSON.parse(response.data).payload
-  Object.keys(joined).forEach(k => Vue.set(connectedUsers, k, joined[k]))
-}
+demultiplexer.addConsumer(presenceConsumer)
 
 export const gameVue = new Vue({
   el: '#app',
